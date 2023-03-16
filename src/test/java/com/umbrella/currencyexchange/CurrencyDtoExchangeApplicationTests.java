584a0f45636e17b8a2b794b1df7f4b7088b81a4b
Aqil Aghamirzayev <aqilzeka99@gmail.com>
2023-03-16 18:20:19 +0400

    Create project

---
+++ src/test/java/com/umbrella/currencyexchange/CurrencyDtoExchangeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.umbrella.currencyexchange;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurrencyDtoExchangeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
